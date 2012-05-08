bdcd7a3564e78c2ffa0c3044c5f82f06b7c4e425
Neil Kamireddy <neil.kamireddy@gmail.com>
2012-05-07 20:29:33 -0500

    updated s3 config for heroku

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,7 +1,1 @@
-if Rails.env == "production"
-  # set credentials from ENV hash
-  S3_CREDENTIALS = { :access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'], :bucket => "Scratchboard"}
-else
-  # get credentials from YML file
-  S3_CREDENTIALS = Rails.root.join("config/s3.yml")
-end
+S3_CREDENTIALS = { :access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'], :bucket => "Scratchboard"}
