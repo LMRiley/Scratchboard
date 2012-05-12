a1c734e72e1cc0f439e2d071d35a694d79f6bc82
Neil Kamireddy <neil.kamireddy@gmail.com>
2012-05-12 14:49:14 -0500

    updated environment file

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,5 +1,5 @@
-if Rails.env == "production"
-  S3_CREDENTIALS = { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'], :bucket => ENV['S3_BUCKET_NAME'] }
-else
+if Rails.env == "development"
   S3_CREDENTIALS = Rails.root.join("config/s3.yml")
+else
+  S3_CREDENTIALS = { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'], :bucket => ENV['S3_BUCKET_NAME'] }
 end
