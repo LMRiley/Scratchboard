8737c402d827d03f2bef89d0ea81104d3a1b8abf
Neil Kamireddy <neil.kamireddy@gmail.com>
2012-05-12 13:04:56 -0500

    updated heroku configs

--- app/models/thought.rb
+++ app/models/thought.rb
@@ -5,14 +5,14 @@ class Thought < ActiveRecord::Base
   validates         :content, :presence => true
   has_attached_file :scratchfile,
       :storage => :s3,
-      :bucket => 'Scratchboard',
+      :bucket => 'scratchboard',
       :s3_credentials => S3_CREDENTIALS,
       :styles => lambda{ |a|
       ["image/jpeg", "image/png", "image/jpg", "image/gif"].include?( a.content_type ) ? {
-        :thumb => "100x100",
+        :thumb  => "100x100",
         :small  => "200x200",
         :medium => "400x400",
-        :large =>   "1000x1000" }: {}
+        :large  => "1000x1000" }: {}
       }
     
   default_scope :order => 'thoughts.created_at DESC'
