9781ab51a41769ca04902bbb2a26bc111070c201
Neil Kamireddy <neil.kamireddy@gmail.com>
2012-05-17 07:46:13 -0500

    testing s3

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,5 +1,1 @@
-if Rails.env == "development"
-  S3_CREDENTIALS = Rails.root.join("config/s3.yml")
-else
-  S3_CREDENTIALS = { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'] }
-end
+S3_CREDENTIALS = Rails.root.join("config/s3.yml")
