0d0192c2c0b56228953dbeaa8cc83e191797f8cb
Neil Kamireddy <neil.kamireddy@gmail.com>
2013-09-15 10:37:39 -0500

    paperclip env updates

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,5 +1,6 @@
 if Rails.env == "development"
-  S3_CREDENTIALS = Rails.root.join("config/s3.yml")
+  #S3_CREDENTIALS = Rails.root.join("config/s3.yml")
+  S3_CREDENTIALS = { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'] }
 else
   S3_CREDENTIALS = { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'] }
 end
